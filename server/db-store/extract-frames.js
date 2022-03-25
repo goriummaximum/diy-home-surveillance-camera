f4b8eeccaa7335d60db49fd13410285d6622446d
goriummaximum <long.hothiengo@hcmut.edu.vn>
2022-03-25 21:00:36 +0700

    update extract large number of imgs

--- server/db-store/extract-frames.js
+++ server/db-store/extract-frames.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const common = JSON.parse(fs.readFileSync(`${__dirname  }/../common.json`));
-const {Pool} = require("pg");
+const { Pool } = require("pg");
+const Cursor = require("pg-cursor");
 
 //change time here
 const from = {
@@ -21,6 +22,11 @@ const to = {
     'ss': '59'
 }
 
+const batch_size = 1000;
+
+let from_unix_timestamp = new Date(`${from['yyyy']}-${from['mm']}-${from['dd']} ${from['hh']}:${from['min']}:${from['ss']}.000`).getTime();
+let to_unix_timestamp = new Date(`${to['yyyy']}-${to['mm']}-${to['dd']} ${to['hh']}:${to['min']}:${to['ss']}.000`).getTime();
+
 const db_pool = new Pool({
     user: common["PG_USERNAME"],
     password: common["PG_PASSWORD"],
@@ -29,45 +35,43 @@ const db_pool = new Pool({
     database: common["PG_DB"]
 });
 
-function extract(from_yyyy, from_mm, from_dd, from_hh, from_min, from_ss, to_yyyy, to_mm, to_dd, to_hh, to_min, to_ss) {
-    let from = new Date(`${from_yyyy}-${from_mm}-${from_dd} ${from_hh}:${from_min}:${from_ss}.000`).getTime();
-    let to = new Date(`${to_yyyy}-${to_mm}-${to_dd} ${to_hh}:${to_min}:${to_ss}.000`).getTime();
+function cursor_read(cursor, batch_size) {
+    cursor.read(batch_size, (err, rows) => {
+        if (rows.length == 0) {
+            console.log("finish!!!");
+            cursor.close();
+        }
 
-    return db_pool.query(
-        `SELECT * FROM public.frame
-        WHERE "timestamp" >= $1 AND "timestamp" <= $2`,
-        [from, to]
-    )
-    .then((res) => {
-        return res.rows;
-    })
-    .catch((err) => {
-        return err;
+        else {
+            rows.forEach(frame => {
+                fs.writeFileSync(`${dir_name}/${frame['timestamp']}.jpg`, frame['frame'], {encoding: "base64", flag: "w"});
+                console.log(`'${frame['timestamp']}.jpg' saved`);
+            })
+
+            cursor_read(cursor, batch_size);
+        }
     });
 }
 
-console.log(`querying...`);
-extract(from['yyyy'], from['mm'], from['dd'], from['hh'], from['min'], from['ss'], to['yyyy'], to['mm'], to['dd'], to['hh'], to['min'], to['ss'])
-.then((frames) => {
+async function extract() {
     let dir_name = `${__dirname}/extract/${Date.now()}`;
     if (!fs.existsSync(dir_name)) {
         fs.mkdirSync(dir_name, { recursive: true });
         console.log(`folder created: '${dir_name}'`);
     }
 
-    frames.forEach(frame => {
-        fs.writeFileSync(`${dir_name}/${frame['timestamp']}.jpg`, frame['frame'], {encoding: "base64", flag: "w"});
-        console.log(`'${frame['timestamp']}.jpg' saved`);
-    })
-})
-.catch((err) => {
-    console.log(err);
-})
-.finally(() => {
-    console.log("finish!");
-});
+    let client = await db_pool.connect();
 
+    let cursor = await client.query(new Cursor(
+        `SELECT * FROM public.frame
+        WHERE "timestamp" >= $1 AND "timestamp" <= $2`,
+        [from_unix_timestamp, to_unix_timestamp]
+    ));
 
+    cursor_read(cursor, batch_size);
+}
+
+extract();
 
 
 
